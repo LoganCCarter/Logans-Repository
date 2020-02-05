9037a05928fe3fd9f5a95c3f8ee8f756b01aebde
LoganCCarter <60410616+LoganCCarter@users.noreply.github.com>
2020-02-05 15:32:21 -0600

    fghidk

---
+++ idk.java
@@ -0,0 +1,26 @@
+class idk{
+public String runLength(String a){
+String runLength = int 42;
+String a = int 12345;
+if(String runLength > String a)
+	
+
+}
+public String runLengthChecker(String 1,String 2){
+String 1 = int 11;
+String 2 = int 22;
+
+
+if (String 1 = true)
+	System.out.print(String 1);
+if (String 2 = true)
+	System.out.print(String 2);
+	
+
+}
+public static void main (String[]o){
+	idk tester =  new idk();
+	String testOne=tester.runLength("Ab");
+	System.out.println(TestOne);
+}
+}
