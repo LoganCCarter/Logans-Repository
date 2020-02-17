4e10e0e96dd83851af37a787b9d2abb8a35936fd
LoganCCarter <60410616+LoganCCarter@users.noreply.github.com>
2020-02-17 11:06:16 -0600

    Add files via upload

---
+++ homework.java
@@ -0,0 +1,45 @@
+class homework{
+	
+	hiddenHomework hidden = new hiddenHomework();
+	
+	String[] stringArray = new String[9];
+	
+	int[] intArray = new int[9];
+	
+	double[] doubleArray = new double[9];
+
+	homework(){
+
+	/*Give the last element of the double array and int array are from methods that has two doubles as the parameter.
+	  the last element of the double array can be found using [doubleArray.length - 1] 
+	  populate that element of the array with the Proper method that has two doubles as the parameter
+	  i.e. doubleArray[doubleArray.length - 1] = hidden.ShowMine(12.3 , 456.789); */
+	
+	//  Give showmine a String that can be autoboxed to a int.  if its even and less than 6 store it in that index... if it odd it won't be right.
+		
+	// Not This is for index 2
+
+	// ShowYours will give you the 0,3,6 index if you pass in the index you want...but only for 0,3,6.
+		
+	// the String at element index 1 and 2 don't have any parameters. first show yours this show mine.
+		
+	// Index 7 is a ShowYours method for all three arrays. They each take in three parameters but not one is the same as the return type.
+	
+	// if you give ShowYours the same two ints you have index five. if they are different you have index six.
+
+	// ShowMine & Showyours with String and int parameters are 5 then 4 repectfully;
+
+	//Give Showyours a String that can be autoboxed to a int.  index 3,6,or 8 will work ... if other numbers won't give the correct value.
+
+	/* if you have Straight A's in school what is your GPA? What about if you have Straight B's 
+	   give ShowsYours your GPA for index 3 if you have B's and 4 if you had A's*/
+	
+	// ShowMine with three numerics are the first and last 
+
+	// this last two ints are a ShowMine that takes an int. input 1  for 5 and intput 5 is for 1.
+
+	// Index one double is last() .
+
+	}
+	
+}
